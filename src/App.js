4cf80358dfd88d0410f7f26e3e3d4552770b1ff4
Leonardo Alcantara <leonardoalc23@gmail.com>
2021-12-08 15:30:50 -0300

    recaptcha implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
+import React, { Component } from 'react';
+import Recaptcha from 'react-recaptcha';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleSubscribe = this.handleSubscribe.bind(this);
+    this.recaptchaLoaded = this.recaptchaLoaded.bind(this);
+    this.verifyCallback = this.verifyCallback.bind(this);
+    this.state = {
+      isVerified: false
+    }
+  }
+
+  recaptchaLoaded() {
+    console.log('captcha succesfully loaded');
+  }
+  handleSubscribe() {
+    if (this.state.isVerified) {
+      alert('You have seccesfully subscribed');
+    } else {
+      alert('Please verify that you are a human');
+    }
+  }
+
+ verifyCallback(response) {
+   if (response) {
+     this.setState({
+       isVerified: true
+     })
+   }
+ }
+ render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          React Recaptcha Demo.
+        </h1>
+        <input type="text" placeholder="email@company.com" />
+          <div className="convert" onClick={this.handleSubscribe}>Subscribe</div>
+          <Recaptcha
+            sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+            render="explicit"
+            onloadCallback={this.recaptchaLoaded} 
+            verifyCallback={this.verifyCallback}
+            />
       </header>
     </div>
   );
+ }
 }
 
 export default App;
